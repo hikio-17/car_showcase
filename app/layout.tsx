4c655041ff22654b1aa6106719cd0435189b1a08
hikio-17 <fajri010217@gmail.com>
2023-09-03 14:22:38 +0700

    create navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Navbar, Footer }from '@/components'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className='relative'>
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   )
